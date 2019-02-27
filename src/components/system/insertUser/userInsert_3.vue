78ea61e4fc995c223a4257350ead46223912d501
qushengxin <383398286@qq.com>
2019-02-27 16:52:25 +0800

    userInsert_3路由跳转

--- src/components/system/insertUser/userInsert_3.vue
+++ src/components/system/insertUser/userInsert_3.vue
@@ -1,6 +1,14 @@
 <template>
+<div id="vueDiv">注册成功！</div>
 </template>
-
+<style>
+#vueDiv{
+	font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+	color:#dcdee2;
+	margin-top:50px;
+	font-size:30px;
+}
+</style>
 <script>
     export default {
         name: "userInsert_3"
