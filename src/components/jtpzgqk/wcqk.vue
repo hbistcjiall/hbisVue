bbd616d79cdf8d3e887df1cd309eb40831f40c04
iwillbe12138 <iwillbe12138@163.com>
2019-03-24 17:14:30 +0800

    集团品种钢情况-完成情况
    集团品种钢情况-完成情况

---
+++ src/components/jtpzgqk/wcqk.vue
@@ -0,0 +1,104 @@
+<template>
+    <div>
+        <Form :label-width="100">
+            <Row>
+                <Col span="4" v-if="switchTime">
+                    <FormItem label="月份：" style="width:120px">
+                        <DatePicker type="month" placeholder="起始月份" :editable="false" :clearable="false"
+                                    v-model="startTime"
+                                    style="width:120px"></DatePicker>
+                    </FormItem>
+                </Col>
+                <Col span="2" style="margin-left: 90px">
+                    <Button @click="getList()" icon="ios-search">查询</Button>
+                </Col>
+            </Row>
+        </Form>
+        <Table :columns="columns" :data="data" border height="500"></Table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "wcqk",
+        data() {
+            return {
+                switchTime: true,
+                startTime: new Date(),
+                columns: [
+                    {
+                        title: "单位",
+                        align: "center",
+                        width: 150,
+                        key: 'companyName',
+                    },
+                    {
+                        title: "钢材总量(吨)",
+                        align: "center",
+                        width: 150,
+                        key: 'totalSteel',
+                    },
+                    {
+                        title: "品种钢总量(吨)",
+                        align: "center",
+                        width: 150,
+                        key: 'totalSteelVarieties',
+                    },
+                    {
+                        title: "品种钢比例(%)",
+                        align: "center",
+                        width: 150,
+                        key: 'scaleSteel',
+                    }, {
+                        title: "特色战略产品(吨)",
+                        align: "center",
+                        width: 150,
+                        key: 'featuresProducts',
+                    }, {
+                        title: "高端产品(吨)",
+                        align: "center",
+                        width: 150,
+                        key: 'highProducts',
+                    }, {
+                        title: "一般品种钢(吨)",
+                        align: "center",
+                        width: 150,
+                        key: 'steelVarieties',
+                    },
+                ],
+                data: []
+            }
+        },
+        mounted() {
+            this.getList();
+        },
+        methods: {
+            getList() {
+                let startTime = 'queryDate=';
+                this.switchTime ? (startTime = startTime + (this.utils.formatMonthStart(this.startTime)).substring(0, 7)) : (startTime = startTime + (this.utils.formatYearStart(this.year)).substring(0, 7));
+                // this.switchTime?(startTime=(startTime+ this.utils.formatdate(this.startTime)).substring(0, 20)):(startTime=startTime+ this.utils.formatYearStart(this.year));
+                fetch(this.$store.state.fetchPath + "/reportTechnology/subsidiaryVarietySteel", {
+                    method: "POST",
+                    headers: this.$store.state.fetchHeader,
+                    body: startTime,
+                    credentials: 'include'
+                }).then((res) => {
+                    if (res.status != 200) {
+                        this.$Message.error('请求失败！');
+                    } else {
+                        return res.text();
+                    }
+                }).then((res) => {
+                    res = res && res.length > 0 ? JSON.parse(res) : [];
+                    console.log(startTime);
+                    window.console.log(res.list);
+                    this.data = res.list;
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
