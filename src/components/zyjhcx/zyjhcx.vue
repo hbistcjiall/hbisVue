53329094b8057df4133388f9d2792dab6bb29c23
jiarsi <jrs1028@163.com>
2019-03-24 23:36:07 +0800

    Update zyjhcx.vue

--- src/components/zyjhcx/zyjhcx.vue
+++ src/components/zyjhcx/zyjhcx.vue
@@ -2,33 +2,33 @@
     <div>
         <Form :label-width="100">
             <Row>
-                <Col span="4">
+                <Col span="6">
                     <FormItem label="品种：" style="width:120px">
                         <Select v-model="zyjhcx.pz" style="width:120px" placeholder="请选择品种" @on-change="getCx">
                             <Option v-for="item in pzData" :value="item.value" :key="item.value">{{ item.label }}</Option>
                         </Select>
                     </FormItem>
                 </Col>
-                <Col span="4">
+                <Col span="6">
                     <FormItem label="产线：" style="width:120px">
                         <Select style="width:120px"  v-model="zyjhcx.cx" placeholder="请选择产线">
                             <Option v-for="item in cxData" :value="item.value" :key="item.value">{{ item.label }}</Option>
                         </Select>
                     </FormItem>
                 </Col>
-                <Col span="4">
+                <Col span="6">
                     <FormItem label="销售主体：" style="width:150px">
                         <Select style="width:120px"  v-model="zyjhcx.xszt" placeholder="请选择销售主体">
                             <Option v-for="item in xsztData" :value="item.value" :key="item.value">{{ item.label }}</Option>
                         </Select>
                     </FormItem>
                 </Col>
-                <Col span="4">
+                <Col span="6">
                     <FormItem label="年份：" style="width:150px">
                         <DatePicker type="year" placeholder="年份" :editable="false" :clearable="false" v-model="zyjhcx.nf" style="width:150px"></DatePicker>
                     </FormItem>
                 </Col>
-                <Col span="4">
+                <Col span="6">
                     <FormItem style="width:150px" label="月份：">
                         <Select style="width:120px"  v-model="zyjhcx.yf" placeholder="请选择月份">
                             <Option value="1">1月</Option>
