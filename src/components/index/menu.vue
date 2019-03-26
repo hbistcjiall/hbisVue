01bbcf6a2ff5a74dea91fc7fce5f712f9855ff49
iwillbe12138 <iwillbe12138@163.com>
2019-03-26 08:59:31 +0800

    menu菜单修改
    menu菜单修改

--- src/components/index/menu.vue
+++ src/components/index/menu.vue
@@ -1,5 +1,5 @@
 <template>
-    <Menu width="180px" ref="side_menu" @on-open-change="getParentName" @on-select="getName" accordion>
+    <Menu width="182px" ref="side_menu" @on-open-change="getParentName" @on-select="getName" accordion>
         <div v-for="(item,index) of menulist" :key="index">
             <MenuItem v-if="!item.children" :name="item.name" >{{item.name}}</MenuItem>
             <Submenu v-if="item.children"  :name="item.name" >
