910dc5acdd08c186ce92f3eb17c9d2bfc0c93ac1
iwillbe12138 <iwillbe12138@163.com>
2019-03-27 17:30:45 +0800

    【用户管理】，添加用户时失败bug，修复
    【用户管理】，添加用户时失败bug，修复

--- src/components/system/userMsg/userMsg.vue
+++ src/components/system/userMsg/userMsg.vue
@@ -324,6 +324,13 @@
                         })
                     },
                     onOk: () => {
+                        if(this.formValidate.birthday==''||this.formValidate.birthday=='NaN-NaN-NaN'){
+                            let birDay = new Date();
+                            this.formValidate.birthday= this.utils.format(birDay)
+                        }else{
+                            this.formValidate.birthday=this.formValidate.birthday
+                        }
+                        alert(this.formValidate.birthday)
                         fetch(this.$store.state.fetchPath + "/mgr/add", {
                             method: "POST",
                             headers: this.$store.state.fetchHeader,
