c0a40853e44888d44fdae7d20c377db6522e380e
Liushizhen <1817444019@qq.com>
2019-05-14 11:38:29 +0800

    state.js
    state.js

--- src/store/state.js
+++ src/store/state.js
@@ -1,10 +1,7 @@
 export default {//全局状态存储
     isLogin:false,//登录状态
     current:0,//新增用户 步骤条
-    // fetchPath:'http://data.hbistc.com',//fetch服务器地址   fuwuqi
-    // fetchPath:'http://18.4.18.104:8081',//fetch服务器地址   ypc
-    fetchPath:'http://18.4.22.0:8081',//fetch服务器地址   zb
-    // fetchPath:'http://18.4.17.200:8081',//fetch服务器地址   ml
+    fetchPath:'http://data.hbistc.com',//fetch服务器地址   fuwuqi
     fetchHeader:{//fetch请求头
         "Content-Type":"application/x-www-form-urlencoded; charset=UTF-8"
     },
